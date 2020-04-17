1ccf94426a62f26d76df65f701f3adcef8ddca9d
Peter Parker <46089498+ParzivialWarrior@users.noreply.github.com>
2020-04-17 14:08:28 -0400

    Add files via upload

--- Castley Dungeons/HealthBar.gd
+++ Castley Dungeons/HealthBar.gd
@@ -1,42 +1,29 @@
 extends Node
 
+
 signal max_changed(new_max)
 signal changed(new_amount)
+signal depleted
+
 export(int)var max_amount = 10 setget set_max
 onready var current = max_amount setget set_current
 
 func _ready():
-#	_intalize()
+	_initalize()
+	
 	
 func set_max(new_max):
 	max_amount = max(1, new_max)
 	emit_signal("max_changed", max_amount)
-	
-	
+
 func set_current(new_value):
-		current = clamp(current, 0, max_amount)
-		emit_signal("changed", current)
-			
-	#if current == 0:
-	#	emit_signal("deplated")
-				
+	current = clamp(current, 0, max_amount)
+	emit_signal("changed", current)
+	
+	if current == 0:
+		emit_signal("depleted")
 		
-func _intialize():
-		emit_signal("max_changed", max_amount)
-		emit_signal("change", current)
-			
-		
-		
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _intitalize():
+	emit_signal("max_changed", max_amount()
+	 emit_signal("changed", current)
+	
--- Castley Dungeons/Player.gd
+++ Castley Dungeons/Player.gd
@@ -1,9 +1,17 @@
 extends KinematicBody
 
 var speed = 8
+var sprint_speed = 10
+var crouch_move_speed = 3
+var crouch_speed = 20
+var default_speed = 7
+var sprinting = false
 var acceleartion = 20
 var gravity = 9.5
-var jump = 10
+var jump = 5
+
+var default_height = 1.5
+var crouch_height = 0.5
 
 var mouse_sensitvity = 0.05 
 
@@ -12,7 +20,9 @@ var velocity = Vector3()
 var fall = Vector3()
 
 onready var head = $head
-
+onready var sprint_timer = $SprintTimer
+onready var pcap = $CollisionShape
+onready var bonker = $HeadBonker
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -22,6 +32,14 @@ onready var head = $head
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("fire"):
+		var direct_state = get_world().direct_space_state
+		var collision = direct_state.intersect_ray(transform.origin, Vector3(0, 0, -20))
+		
+		if collision:
+			print(collision.position)
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -33,16 +51,42 @@ func _input(event):
 		
 func _process(delta):
 	
-	direction = Vector3()
+	#var head_bonked = false
 	
+	speed = default_speed
+	
+	direction = Vector3()
+	#if bonker.is_colliding():
+	#	head_bonked = true
+		
 	if not is_on_floor():
 		fall.y -= gravity * delta
 		
 	if Input.is_action_pressed("jump"):
 		fall.y = jump
+	#if head.bonked:
+		#fall.y = 2
+	
+	if Input.is_action_pressed("ability") and not sprinting:
+		sprinting = true
+		sprint_timer.start()
+	elif Input.is_action_just_pressed("ability") and not sprinting:
+		sprinting = false
+	
+	if sprinting:
+		speed = sprint_speed
+		
+	if Input.is_action_pressed("crouch"):
+		pcap.shape.height -= crouch_speed * delta
+		speed = crouch_move_speed
+	else:
+		pcap.shape.height += crouch_speed * delta
+	
+	pcap.shape.height = clamp(pcap.shape.height, crouch_height, default_height)
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_move_model(Input.MOUSE_MODE_VISIBLE)
-	
+		
 	if Input.is_action_pressed("move_forward"):
 		direction -= transform.basis.z
 		
@@ -54,6 +98,10 @@ func _process(delta):
 
 	elif Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
+		
+	if Input.is_action_pressed("attack"):
+		
+		print("hello world")
 	
 	direction = direction.normalized()
 	velocity = velocity.linear_interpolate(direction * speed, acceleartion * delta)
@@ -64,3 +112,8 @@ func _process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 	#pass
+
+
+func _on_Sprint_Timer_timeout():
+	sprinting = false
+
